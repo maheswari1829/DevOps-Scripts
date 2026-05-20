dd5037c466aa5bcffdb35bc917362139e6ff7c7c
Bhavani Maheswari Chavali <maahi8125@gmail.com>
2026-05-20 19:41:41 +0530

    Create grafana-installation.sh

---
+++ Monitoring/grafana-installation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ----------------------------------------
+# GRAFANA INSTALLATION SETUP
+# ----------------------------------------
+
+# Install required dependencies
+sudo apt-get install -y adduser libfontconfig1
+
+# Download Grafana Enterprise package
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise_9.4.7_amd64.deb
+
+# Install Grafana package
+sudo dpkg -i grafana-enterprise_9.4.7_amd64.deb
+
+# Reload systemd daemon
+sudo systemctl daemon-reload
+
+# Enable Grafana service
+sudo systemctl enable grafana-server
+
+# Start Grafana service
+sudo systemctl start grafana-server
+
+# Verify Grafana service status
