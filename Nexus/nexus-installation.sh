654d596b6542f48d0b257914742bf44c918cb665
Bhavani Maheswari Chavali <maahi8125@gmail.com>
2026-05-20 18:54:45 +0530

    Create nexus-installation.sh

---
+++ Nexus/nexus-installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install Java 17
+sudo yum install java-17-amazon-corretto -y
+
+# Download Nexus package
+wget https://download.sonatype.com/nexus/3/nexus-unix-x86-64-3.79.0-09.tar.gz
+
+# Extract Nexus package
+tar -zxvf nexus-unix-x86-64-3.79.0-09.tar.gz
+
+# Create Nexus user
+sudo useradd nexus
+
+# Change ownership
+sudo chown -R nexus:nexus nexus-3.79.0-09
+
+# Start Nexus
+sudo sh nexus-3.79.0-09/bin/nexus start
