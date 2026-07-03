69542c2df3fb76c44a39db16cda3f8fdcd0c43c6
Bhavani Maheswari Chavali <maahi8125@gmail.com>
2026-07-03 10:34:11 +0530

    Update  docker-compose-installation.sh

--- Docker/ docker-compose-installation.sh
+++ Docker/ docker-compose-installation.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Download Docker Compose binary
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.40.3/docker-compose-linux-x86_64 \
--o /usr/local/bin/docker-compose
+sudo curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o docker-compose
+
+# Check the file
+ls -lh docker-compose
 
 # Give executable permissions
-sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod +x docker-compose
 
-# Create symbolic link
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+# Verify
+ls -l docker-compose
 
 # Verify installation
 docker-compose version
