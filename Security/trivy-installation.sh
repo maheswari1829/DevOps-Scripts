79bdfc8ac0b4cf64b35822924dcc165f6e91d2c7
Bhavani Maheswari Chavali <maahi8125@gmail.com>
2026-07-13 09:33:14 +0530

    Update trivy-installation.sh

--- Security/trivy-installation.sh
+++ Security/trivy-installation.sh
@@ -5,10 +5,10 @@
 # ----------------------------------------
 
 # Download Trivy package
-wget https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.tar.gz
+wget https://github.com/aquasecurity/trivy/releases/download/v0.72.0/trivy_0.72.0_Linux-64bit.tar.gz
 
 # Extract Trivy package
-tar -zxvf trivy_0.18.3_Linux-64bit.tar.gz
+tar -zxvf trivy_0.72.0_Linux-64bit.tar.gz
 
 # Move Trivy binary to system path
 sudo mv trivy /usr/local/bin/
